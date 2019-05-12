497dd11e32bac4339ba5b612f5f93567edadab5b
JAcoder7 <Jaho24@outlook.de>
2019-05-12 09:36:39 +0200

    add progressbar

--- NJdesign.js
+++ NJdesign.js
@@ -1,1 +1,8 @@
 /* The JavaScript from the NJ-web-design */
+
+function progressbar(id, width, progress) {
+    let progressbar = document.getElementById(id);
+    progressbar.style = "width: " + width + "pt;";
+    progressbar.innerHTML = "<div id='" + id + "b'></div>";
+    document.getElementById(id + "b").style = "width: " + width/100*progress + "pt;";
+}
--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # NJ-web-design
 A Design-API inspirited from Microsoft Fluent Design
 
-*branch from Ncoder77*
-
-**My changes:**  
+###Progressbar
+To add a progessbar
