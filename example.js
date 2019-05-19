c930e647e7501885ddc6649742bb62c0a79a7bc9
JaCoder7 <jaho24@outlook.de>
2019-05-19 09:53:05 +0200

    removed old files

--- example.js
+++
@@ -1,6 +0,0 @@
-/* WARNING: This is a second js file for your own javascript.
- * The default NJDesign javascript for menus, progressBars etc. are in
- * the NJDesign.js file. THis is just an example.
- */
-
-//type your js here:
